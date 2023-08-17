1c4f452cf6c6647547711f0ffe68a837f3c453c4
OMHall <81428354+OMHall@users.noreply.github.com>
2023-08-18 00:26:26 +0200

    Create parameters.py

---
+++ CAN/parameters.py
@@ -0,0 +1,40 @@
+# Root directory for dataset
+dataroot = './wikiart_16_9/'
+# Number of workers for dataloader
+workers = 2
+
+# Batch size during training
+batch_size = 16
+# smaller batchsize more stable training
+
+# Number of styles
+n_class = 27
+
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+image_size_a = 256
+image_size_b = 144
+
+# Number of channels in the training images. For color images this is 3
+nc = 3
+
+# Size of z latent vector (i.e. size of generator input)
+nz = 100
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discriminator
+ndf = 32
+
+# Number of training epochs
+num_epochs = 100
+
+# Learning rate for optimizers
+lr = 0.0002
+
+# Beta1 hyperparam for Adam optimizers
+beta1 = 0.5
+
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = 1
